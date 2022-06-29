c49b2e5db51bbe2826463eef7d298bfbcd921151
Andrew <andrewnijmeh1@gmail.com>
2022-06-29 11:42:57 -0400

    config(tailwind): font-family ibm plex sarif

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
+    "./animations/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
+    fontFamily: {
+      sans: ["IBM Plex Sarif", ...fontFamily.sans],
+    },
 		extend: {},
 	},
 	plugins: [],
