f1f13059cb84e79fb088080a4d28339c02d8c153
andrew <andrewnijmeh1@gmail.com>
2022-10-04 13:50:22 -0400

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [react(), tailwind()]
 });
---
+++ src/utils/time.ts
@@ -0,0 +1,12 @@
+// all credit to alistair.sh for this code
+const bday = new Date('1 June 2008 00:07:00 GMT');
+const ageMilliseconds = Date.now() - bday.getTime();
+
+export const MILLISECOND = 1;
+export const SECOND = MILLISECOND * 1000;
+export const MINUTE = SECOND * 60;
+export const HOUR = MINUTE * 60;
+export const DAY = HOUR * 24;
+export const YEAR = DAY * 365;
+
+export const age = ageMilliseconds / YEAR;
