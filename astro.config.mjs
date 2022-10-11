9652365d9c4e8602130013e4ef5b63811961aac1
andrew <andrewnijmeh1@gmail.com>
2022-10-11 09:48:48 -0400

    remove cloudflare adapter to fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
   integrations: [react(), tailwind()]
 });
