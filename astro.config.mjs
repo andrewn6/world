0e2c0a069014d3bc7e4de3c1e4c5c6ffaa397fe2
andrew <andrewnijmeh1@gmail.com>
2022-10-11 10:10:32 -0400

    i have no idea what i am doing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), tailwind()],
-  adapter: vercel()
 });
