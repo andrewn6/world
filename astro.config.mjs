5fc0fbf3388caf1d375fa80e415650bb2a85b8b0
andrew <andrewnijmeh1@gmail.com>
2022-10-11 10:04:01 -0400

    reinstall node_modules + add astro vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: vercel()
 });
