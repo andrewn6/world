0bac4e653b6a8eb9260a6a1f46eb42975a17cc09
andrew <andrewnijmeh1@gmail.com>
2022-10-17 20:01:07 -0400

    waving hand emoji + create server package

---
+++ server/target/debug/deps/server-2944f3fda09df188.d
@@ -0,0 +1,7 @@
+/Users/andrew/github/anddddrew/public/Astro/nijmeh.cloud/server/target/debug/deps/server-2944f3fda09df188.rmeta: src/main.rs
+
+/Users/andrew/github/anddddrew/public/Astro/nijmeh.cloud/server/target/debug/deps/server-2944f3fda09df188.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
---
+++ server/target/debug/deps/server-a56002d59067222e.d
@@ -0,0 +1,7 @@
+/Users/andrew/github/anddddrew/public/Astro/nijmeh.cloud/server/target/debug/deps/server-a56002d59067222e.rmeta: src/main.rs
+
+/Users/andrew/github/anddddrew/public/Astro/nijmeh.cloud/server/target/debug/deps/server-a56002d59067222e.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
