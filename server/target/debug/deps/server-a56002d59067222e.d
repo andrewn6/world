09bb18dc540616f67f6506cc01f0e1ba5ccbc8f8
andrew <andrewnijmeh1@gmail.com>
2022-10-18 10:18:44 -0400

    create base rocket server + add meta embed

--- server/target/debug/deps/server-a56002d59067222e.d
+++
@@ -1,7 +0,0 @@
-/Users/andrew/github/anddddrew/public/Astro/nijmeh.cloud/server/target/debug/deps/server-a56002d59067222e.rmeta: src/main.rs
-
-/Users/andrew/github/anddddrew/public/Astro/nijmeh.cloud/server/target/debug/deps/server-a56002d59067222e.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
