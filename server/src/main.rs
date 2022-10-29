7f9a9d4a32c8be05d43179c0a8c47c9fb613a1d5
andrew <andrewnijmeh1@gmail.com>
2022-10-29 11:20:49 -0400

    change file name for workflow

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,27 +2,27 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+	HttpResponse::Ok().body("Hello world!")
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+	HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("andrew nijmeh")
+	HttpResponse::Ok().body("andrew nijmeh")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8000))?
-    .run()
-    .await
+	HttpServer::new(|| {
+		App::new()
+			.service(hello)
+			.service(echo)
+			.route("/hey", web::get().to(manual_hello))
+	})
+	.bind(("127.0.0.1", 8000))?
+	.run()
+	.await
 }
