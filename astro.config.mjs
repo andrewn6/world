26b33a922b64707ffd530fd68a905759bbe14209
andrew <andrewnijmeh1@gmail.com>
2022-11-10 15:55:36 -0500

    feat(404 page)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: 'https://nijmeh.cloud',
+  integrations: [react(), tailwind(), mdx()],
 });
