400a73db82088b7a5251b54f62f36e70b0115a59
andrew <andrewnijmeh1@gmail.com>
2022-11-19 15:39:03 -0500

    connect to do spaces via s3 client

--- cdn/go.mod
+++ cdn/go.mod
@@ -6,6 +6,8 @@ require github.com/gofiber/fiber/v2 v2.40.0
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aws/aws-sdk-go v1.44.142 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -14,5 +16,5 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.41.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
--- cdn/main.go
+++ cdn/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
   "log"
+  "os"
+  "fmt"
+  "strings"
 
   "github.com/gofiber/fiber/v2"
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/credentials"
+  "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/s3"
   "nijmeh.cloud/routes"
 )
 
@@ -12,6 +19,35 @@ func setupRoutes(app *fiber.App) {
 }
 
 func main() {
+  key := "DO008YGT3UV8M3GNA6Z2"
+  secret := os.Getenv("SPACES_SECRET")
+  
+  s3Config := &aws.Config {
+    Credentials: credentials.NewStaticCredentials(key, secret, ""),
+    Endpoint: aws.String("https://nyc3.digitaloceanspaces.com"),
+    S3ForcePathStyle: aws.Bool(false),
+    Region: aws.String("us-east-1"),
+  }
+  
+  newSession := session.New(s3Config)
+  s3Client := s3.New(newSession)
+  
+  object := s3.PutObjectInput {
+    Bucket: aws.String("nijmeh"),
+    Key: aws.String("folder/flag-orpheus-top.svg"),
+    Body: strings.NewReader("Hello, World!"),
+    ACL: aws.String("private"),
+    Metadata: map[string]*string{
+                            "x-amz-meta-my-key": aws.String("your-value"),
+                    },
+  }
+
+  _, err := s3Client.PutObject(&object)
+  if err != nil {
+    fmt.Println(err.Error())
+  }
+  
+
   app := fiber.New()
 
   setupRoutes(app)
