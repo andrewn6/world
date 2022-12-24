a515d3f2c8696a3842e3035145833d754cc7fe93
andrew <andrewnijmeh1@gmail.com>
2022-12-24 13:25:16 -0500

    add age util (credit to @alii)

---
+++ src/utils/time.ts
@@ -0,0 +1,7 @@
+// credit to alistair.sh
+
+const birth = Date.parse(new Date('1 June 2008 12:00 EST').toString());
+
+const msSinceBirth = Date.parse(Date()) - birth;
+
+export const age = msSinceBirth / 1000 / 60 / 60 / 24 / 365;
